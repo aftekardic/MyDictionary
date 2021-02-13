584509d8ef57ab1ee696775add05350dc676a935
aftekardic <65916908+aftekardic@users.noreply.github.com>
2021-02-13 12:34:03 +0300

    Add files via upload

---
+++ DictonaryGenerics/MyDictonary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictonaryGenerics
+{
+    class MyDictonary<K,V>
+    {
+        K[] _keyArray;
+        V[] _valueArray;
+
+        //create contructor
+        public MyDictonary()
+        {
+            _keyArray = new K[0];
+            _valueArray = new V[0];
+        }
+
+        public void Add(K key , V value)
+        {
+            K[] tempKeyArray = _keyArray;
+            V[] tempValueArray = _valueArray;
+
+            _keyArray = new K[_keyArray.Length + 1];
+            _valueArray = new V[_valueArray.Length + 1];
+
+            // for loop with key
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                _keyArray[i] = tempKeyArray[i]; 
+            }
+            _keyArray[_keyArray.Length - 1] = key;
+
+            //for loop with value
+            for (int j = 0; j < tempValueArray.Length; j++)
+            {
+                _valueArray[j] = tempValueArray[j];
+            }
+            _valueArray[_valueArray.Length - 1] = value;
+        }
+
+    }
+}
---
+++ DictonaryGenerics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictonaryGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> Kisiler = new Dictionary<int, string>();
+            Kisiler.Add(0, "Ali Furkan");
+
+            MyDictonary<int, string> Deneme = new MyDictonary<int, string>();
+            Deneme.Add(2, "Mehmet");
+            Console.WriteLine(Deneme);
+        }
+    }
+}
